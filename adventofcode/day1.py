476bcf17574c7c3b0e6d8136a4edbedb89ea1a8c
Julien Le Sauce <julien.lesauce@gmail.com>
2023-01-20 15:04:50 +0100

    Day 1 - Part 2

--- adventofcode/day1.py
+++ adventofcode/day1.py
@@ -7,15 +7,32 @@ from adventofcode.inputs.input_file import InputFile
 def main() -> int:
     print('-- Day 1 --')
 
+    elves_calories = part1()
+    part2(elves_calories)
+
+    return 0
+
+
+def part1() -> list:
     elves_calories = collect_all_elves_calories(InputFile('day1.dat').path)
     elf_index_with_max_calories = elves_calories.index(max(elves_calories))
 
     print(f'Elf with highest calories amount is {elf_index_with_max_calories},'
           f'who has {elves_calories[elf_index_with_max_calories]} calories.')
 
+    print(f'\nAnswer (Part 1): {elves_calories[elf_index_with_max_calories]}\n')
+
+    return elves_calories
+
+
+def part2(elves_calories: list):
+    print('The top 3 of the highest amount of calories is:\n'
+          f'1. {elves_calories[0]}\n'
+          f'2. {elves_calories[1]}\n'
+          f'3. {elves_calories[2]}')
+
     print('')
-    print(f'Answer: {elves_calories[elf_index_with_max_calories]}')
-    return 0
+    print(f'Answer (Part 2): {sum(elves_calories[0:3])}')
 
 
 def collect_all_elves_calories(input_file_path: Path) -> list:
---
+++ adventofcode/day2.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def main() -> int:
+    print('-- Day 2 --')
+
+    print(f'\nAnswer: ')
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
